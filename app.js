5571b80324d794d2d6552eec76622e9da7a547e1
cmattey <cmattey@dons.usfca.edu>
2020-02-08 17:15:52 -0800

    Refactored App into separate modules

---
+++ app.js
@@ -0,0 +1,26 @@
+const config = require('./utils/config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+
+console.log('connecting to db');
+
+mongoose.set('useFindAndModify', false);
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  })
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
