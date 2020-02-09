18ce4653ba2348e22d57be631b832948dec8b49c
cmattey <cmattey@dons.usfca.edu>
2020-02-08 17:16:24 -0800

    minor updates

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('well whale veil')
+const app = require('./app')
+const config = require('./utils/config');
+
+const PORT = config.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
