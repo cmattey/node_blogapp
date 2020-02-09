98da18d25f69ea87fd4aa1486033bc3e418b4bc5
cmattey <cmattey@dons.usfca.edu>
2020-02-08 21:39:19 -0800

    Ex4.4 Added tests for totallikes on blogs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogList) => {
+
+  const reducer = (sum, item) => {
+    return sum+item.likes;
+  }
+
+  likes = blogList.reduce(reducer, 0)
+  return isNaN(likes)? 0: likes;
+
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
