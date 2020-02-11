7eaa2bdde5406e5d1e1a453bc91b5f78843d43f2
cmattey <cmattey@dons.usfca.edu>
2020-02-10 23:17:28 -0800

    added usersRouter to handle users api endpoints

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 
 console.log('connecting to db');
@@ -22,5 +23,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
