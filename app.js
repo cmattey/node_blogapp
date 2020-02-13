77a7ed83552bcec6a0c6a954b72ffc266aae9b72
cmattey <cmattey@dons.usfca.edu>
2020-02-12 18:19:03 -0800

    Ex 4.18 added login router and token authentication

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
+
 
 console.log('connecting to db');
 
@@ -22,6 +24,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use('/api/login', loginRouter);
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 
